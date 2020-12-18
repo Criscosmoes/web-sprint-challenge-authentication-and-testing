a0cede1da8482640ec1c78f92e2423216915a6d5
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-12-17 22:10:23 -0800

    starting tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,32 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const request = require("supertest"); 
+const server = require("./server"); 
+const db = require("../data/dbConfig"); 
+
+
+const Sam = { name: "Sam", password: "thisisatest" }; 
+const Frodo = { name: "Frodo" }; 
+const Cristian = { name: "Cristian" }; 
+
+
+beforeAll(async () => {
+  await db.migrate.rollback(); 
+  await db.migrate.latest(); 
 })
+
+beforeEach(async () => {
+  await db("users").truncate(); 
+})
+
+afterAll(async () => {
+  await db.destroy(); 
+})
+
+describe("endpoints", () => {
+  describe("[POST] /api/auth/register", () => {
+    it('returns newly created user', async () => {
+      const res = await request(server).post("/api/auth/register").send(Sam); 
+      console.log(res.body); 
+    })
+  })
+})
+
